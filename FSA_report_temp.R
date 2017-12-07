8efe473463700e42ccc58430b24b21bd433287c1
wittec <witte@iastate.edu>
2017-12-07 14:03:34 -0600

    Adding files

---
+++ FSA_report_temp.R
@@ -0,0 +1,385 @@
+# setwd("vignettes/")
+
+rm(list=ls(all=TRUE))
+
+library("dplyr")
+library("ggplot2")
+
+# Rain
+rain <- STRIPS2Helmers::rain %>%
+  mutate(treatment = ifelse(grepl("ctl", watershed), "control", "treatment"),
+         site = gsub("ctl", "", watershed),
+         site = gsub("trt", "", site),
+         year = lubridate::year(date_time)) %>%
+  rename(rain = `rain-m`) %>%
+  group_by(site,year) %>%
+  arrange(date_time) %>%
+  mutate(cumulative_rain = cumsum(rain * 39.3701), # convert to inches
+         watershed_year = paste(watershed,year,sep="_"))
+
+# ggplot(rain, aes(x = date_time, y = cumulative_rain, 
+#               group = watershed_year)) +
+#   geom_line() + 
+#   facet_grid(site ~ year, scales = 'free_x') + 
+#   labs(x = '', y = 'Cumulative rainfall (inches)') + 
+#   theme_bw()
+
+
+# Flow
+myrain <- rain %>%
+  select(-watershed, -watershed_year, -treatment, -cumulative_rain)
+
+
+
+flow <- STRIPS2Helmers::runoff %>%
+  filter(!is.na(flow)) %>%
+  
+  mutate(treatment = ifelse(grepl("ctl", watershed), "control", "treatment"),
+         site = gsub("ctl", "", watershed),
+         site = gsub("trt", "", site),
+         year = lubridate::year(date_time)) %>%
+  
+  left_join(myrain, by=c("date_time", "site", "year")) %>%
+  filter(!is.na(flow), !is.na(rain)) %>%
+  group_by(watershed,year) %>%
+  do(HelmersLab::clip_flow(.)) %>%
+  
+  left_join(readr::read_csv("../data-raw/sitenamesandwatershedsizes.csv")) %>%
+  group_by(watershed,year) %>%
+  mutate(
+    cumulative_flow = cumsum(flow) * 231 * 5 / # convert gpm to in^3 from 5 minutes
+           (acres * 6.273e6) )                 # normalize by watershed area
+                                               # after converting acres to square inches
+
+ggplot(flow, aes(x = date_time, y = cumulative_flow, 
+              group = watershed, linetype = treatment)) +
+  geom_line(size=3) + 
+  facet_grid(site~year, scales='free') + 
+  theme_bw()
+  
+# ggplot(flow, aes(x = date_time, y = cumulative_flow, 
+#               group = watershed, linetype = treatment)) +
+#   geom_line(size=3) + 
+#   facet_grid(site ~ year, scales='free') + 
+#   theme_bw()
+
+
+
+
+# Combine flow and rain
+rain <- rain %>%
+  mutate(treatment = "rain",
+         watershed = paste(site,"_rain", sep=""),
+         y = cumulative_rain)
+
+flow <- flow %>%
+  mutate(y = cumulative_flow)
+
+wnames <- data.frame(site = c("arm","eia","marsh","mcnay","rhodes","spirit","white","worle"),
+                     full = c("Armstrong","E. IA Airport","Marshaltown","McNay",
+                              "Rhodes","Spirit Lake ","Whiterock","Worle"))
+
+d <- bind_rows(flow,rain) %>%
+  mutate(treatment = factor(treatment, 
+                            levels = c("rain","control","treatment"))) %>%
+  left_join(wnames)
+
+
+
+# Customized colors
+colorscale <- c(rain = "blue", 
+                control = "black",
+                treatment = "seagreen")
+linescale <- c(rain = "dotted",
+               control = "solid",
+               treatment = "dashed")
+
+
+
+
+g <- ggplot(d, aes(x = date_time, 
+                   y = y, 
+                   group = watershed, 
+                   linetype = treatment,
+                   color = treatment)) +
+  geom_line() + 
+  facet_grid(full~year, scales='free_x') + 
+  labs(x = '',  
+       y = 'Cumulative rainfall and runoff (inches)') + 
+  scale_color_manual(values = colorscale) +
+  scale_linetype_manual(values = linescale) +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        legend.title    = element_blank())
+
+ggsave(filename = "runoff.jpg", plot=g, width = 6, height=8)
+
+
+# Multiply flow by 10
+g <- ggplot(d %>% left_join(wnames), aes(x = date_time, 
+                                         y = y*ifelse(treatment != "rain", 10, 1), 
+                                         group = watershed, 
+                                         linetype = treatment,
+                                         color = treatment)) +
+  geom_line() + 
+  facet_grid(full~year, scales='free_x') + 
+  labs(x = '',  
+       y = 'Cumulative runoff (x10) and rainfall in inches') + 
+  scale_color_manual(values = colorscale) +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        legend.title    = element_blank())
+
+ggsave(filename = "runoff_x10.jpg", plot=g, width = 6, height=8)
+
+
+
+
+###################################################
+# Sediment
+###################################################
+sed <- STRIPS2Helmers::runoff %>%
+  
+  mutate(treatment = ifelse(grepl("ctl", watershed), "control", "treatment"),
+         site = gsub("ctl", "", watershed),
+         site = gsub("trt", "", site),
+         year = lubridate::year(date_time)) %>%
+  
+  left_join(myrain, by=c("date_time", "site", "year")) %>%
+  # filter(!is.na(flow), !is.na(rain_m)) %>%
+  group_by(watershed,year) %>%
+  do(HelmersLab::clip_flow(.))  # should we be using do() here?
+
+# mutate(sampleID = as.numeric(sampleID)) %>%
+# group_by(watershed,year) %>%
+
+# Remove any watershed-year where no sampleIDs exist
+# mutate(anySampleID = any(!is.na(sampleID))) %>%
+# filter(anySampleID) %>%
+
+sed2 <- sed %>%
+  group_by(watershed,year) %>%
+  #HelmersLab::spread_sampleID() # should we be using do() here?
+
+  do(HelmersLab::spread_sampleID(.)) %>% 
+  filter(!is.na(sampleID), !is.na(flow)) %>%
+  left_join(STRIPS2Helmers::water_quality, by = "sampleID") %>%
+  tidyr::gather(analyte, value,
+                `Nitrate + nitrite (mg N/L)`,
+                `Orthophosphate (mg P/L)`,
+                `TSS (mg/L)`) %>%
+  left_join(readr::read_csv("../data-raw/sitenamesandwatershedsizes.csv")) %>%
+  mutate(valueload = ((value*flow*5)/453592.37)/acres) %>%  #this converts the mg/L units of values into lbs/acre
+  group_by(watershed, year, analyte) %>%
+  filter(!is.na(value)) %>%
+  mutate(cumulative = cumsum(valueload)) %>%
+  ##########################################
+#ADDING THIS STUFF IN
+
+
+#############################################
+  left_join(wnames)
+
+
+
+########################################
+#testing stuff below
+
+test1 <- sed2 %>%
+  group_by(watershed, analyte, year)%>%
+  full_join(test7)%>%
+  arrange(watershed, analyte, date_time)%>%
+  na.locf(, fromlast = T)
+  
+  #complete(watershed, date_time)
+
+
+test2 <- as.data.frame(unique(sed2$date_time)) %>%
+  rename(date_time = 'unique(sed2$date_time)')
+
+test8 <- as.data.frame(seq(start(sed2$date_time, end(sed2$date_time, "5 min"))))
+
+
+az <- as.data.frame(zoo(1:6))
+
+bz <- as.data.frame(zoo(c(2,NA,1,4,5,2)))
+bz1 <- na.locf(bz)
+bz2 <- na.locf(bz, fromLast = TRUE)
+
+z <- as.data.frame(zoo(c(0.007306621, 0.007659046, 0.007681013,
+           0.007817548, 0.007847579, 0.007867313),
+         as.Date(c("1993-01-01", "1993-01-09", "1993-01-16",
+                   "1993-01-23", "1993-01-30", "1993-02-06"))))
+g <-  seq(start(z), end(z), "day")
+na.locf(z, xout = g)
+
+
+
+
+# test2 <- as.data.frame(seq(ISOdate(2016,4,26), ISOdate(2017,6,19), by = "5 min")) %>%
+#   rename(date_time = "seq(ISOdate(2016, 4, 26), ISOdate(2017, 6, 19), by = \"5 min\")")
+#   
+#   # 
+# test3 <- merge(test2, test1, by = "date_time", all.x = T) %>%
+#   arrange(watershed, year, date_time)
+#   
+#   colnames(date)
+#   
+# test4 <- sed2 %>%
+#   mutate(date_time = as.factor(date_time),
+#          analyte = as.factor(analyte),
+#          watershed = as.factor(watershed)) %>%
+#   complete(analyte, watershed, date_time)
+# 
+# df <- tibble(
+#   group = c(1:2, 1),
+#   item_id = c(1:2, 2),
+#   item_name = c("a", "b", "b"),
+#   value1 = 1:3,
+#   value2 = 4:6
+# )
+# 
+# test5 <- df %>%
+#   complete(group, nesting(item_id, item_name))
+
+test7 <- expand.grid(test6$watershed, test2$date_time) %>%
+  rename(watershed = 'Var1', date_time = 'Var2')
+
+test6<- as.data.frame(unique(test1$watershed)) %>%
+  rename(watershed = `unique(test1$watershed)`)
+
+no3graph <- ggplot(test3 %>% 
+         filter(analyte == "Nitrate + nitrite (mg N/L)"), 
+       aes(x = date_time, 
+           y = cumulative,
+           group = treatment,
+           color = treatment,
+           linetype = treatment)) + 
+  geom_line() + 
+  scale_color_manual(values = colorscale) +
+  scale_linetype_manual(values = linescale) +
+  facet_grid(full ~ year, scales = 'free_x') + 
+  labs(x = '',  
+       y = 'Cumulative Nitrate + Nitrite (lbs/ac)') + 
+  theme_bw() +
+  theme(legend.position = "bottom",
+        legend.title    = element_blank())
+
+ggsave(filename = "testno3.jpg", plot=no3graph, width = 6, height=8)
+
+
+#stuff between ### is only stuff I am messing with
+##############################################################################
+
+orthopgraph <- ggplot(sed2 %>% 
+         filter(analyte == "Orthophosphate (mg P/L)"), 
+       aes(x = date_time, 
+           y = cumulative,
+           group = treatment,
+           color = treatment,
+           linetype = treatment)) + 
+  geom_line() + 
+  scale_color_manual(values = colorscale) +
+  scale_linetype_manual(values = linescale) +
+  facet_grid(full ~ year, scales = 'free_x') + 
+  labs(x = '',  
+       y = 'Cumulative Orthophosphate (lbs/ac)') + 
+  theme_bw() +
+  theme(legend.position = "bottom",
+        legend.title    = element_blank())
+
+ggsave(filename = "orthop.jpg", plot=orthopgraph, width = 6, height=8)
+
+
+tssgraph <- ggplot(sed2 %>% 
+         filter(analyte == "TSS (mg/L)"), 
+       aes(x = date_time, 
+           y = cumulative,
+           group = treatment,
+           color = treatment,
+           linetype = treatment)) + 
+  geom_line() + 
+  scale_color_manual(values = colorscale) +
+  scale_linetype_manual(values = linescale) +
+  facet_grid(full ~ year, scales = 'free_x') + 
+  labs(x = '',  
+       y = 'Cumulative Total Suspended Solids (lbs/ac)') + 
+  theme_bw() + 
+  theme(legend.position = "bottom",
+      legend.title    = element_blank())
+
+ggsave(filename = "tss.jpg", plot=tssgraph, width = 6, height=8)
+
+#############################################################################
+#THIS IS FOR lISA TO SEND TO EIA, edited to give to hoien at spirit lake
+
+spirit <- d %>%
+  filter(site=="spirit")
+
+spiritrainrunplot <- ggplot(spirit, aes(x = date_time, 
+                   y = y, 
+                   group = watershed, 
+                   linetype = treatment,
+                   color = treatment)) +
+  ggtitle("Rain and Surface Runoff") +
+  geom_line() + 
+  facet_grid(full~year, scales='free_x') + 
+  labs(x = '',  
+       y = 'Cumulative rainfall and runoff (inches)') + 
+  scale_color_manual(values = colorscale) +
+  scale_linetype_manual(values = linescale) +
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(legend.position = "bottom",
+        legend.title    = element_blank())
+
+ggsave(filename = "spiritrunoff.jpg", plot=spiritrainrunplot, width = 6, height=8)
+
+
+spiritsed2 <- sed2%>%
+  filter(site=="spirit")
+
+spiritorthopgraph <- ggplot(spiritsed2 %>% 
+                        filter(analyte == "Orthophosphate (mg P/L)"), 
+                      aes(x = date_time, 
+                          y = cumulative,
+                          group = treatment,
+                          color = treatment,
+                          linetype = treatment)) + 
+  ggtitle("Surface Runoff Orthophosphate") +
+  geom_line() + 
+  scale_color_manual(values = colorscale) +
+  scale_linetype_manual(values = linescale) +
+  facet_grid(full ~ year, scales = 'free_x') + 
+  labs(x = '',  
+       y = 'Cumulative Orthophosphate (lbs/ac)') + 
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(legend.position = "bottom",
+        legend.title    = element_blank())
+
+ggsave(filename = "spiritorthop.jpg", plot=spiritorthopgraph, width = 6, height=8)
+
+
+spirittssgraph <- ggplot(spiritsed2 %>% 
+                     filter(analyte == "TSS (mg/L)"), 
+                   aes(x = date_time, 
+                       y = cumulative,
+                       group = treatment,
+                       color = treatment,
+                       linetype = treatment)) + 
+  ggtitle("Surface Runoff Sediment") +
+  geom_line() + 
+  scale_color_manual(values = colorscale) +
+  scale_linetype_manual(values = linescale) +
+  facet_grid(full ~ year, scales = 'free_x') + 
+  labs(x = '',  
+       y = 'Cumulative Total Suspended Solids (lbs/ac)') + 
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(legend.position = "bottom",
+        legend.title    = element_blank())
+
+ggsave(filename = "spirittss.jpg", plot=spirittssgraph, width = 6, height=8)
+
+
---
+++ groundwater.R
@@ -0,0 +1,367 @@
+# setwd("vignettes/")
+
+library("dplyr")
+library("tidyr")
+library("ggplot2")
+
+gw2016 <- xlsx::read.xlsx("../data-raw/water_quality/2016/CopyofSTRIPSgroundwaternitrateresultsv2.xlsx", sheetName = "Results", startRow = 22) %>%
+  select(ID..., NOx.result..mg.N.L.) %>%
+  rename(id = ID..., no3mgL = NOx.result..mg.N.L.) %>%
+  mutate(id = as.numeric(as.character(id))) %>%
+  filter(!is.na(id))
+
+
+gw2016a <- xlsx::read.xlsx("../data-raw/water_quality/2016/CopyofSTRIPSgroundwaternitrateresults.xlsx", sheetName = "Results", startRow = 21) %>%
+  select(ID..., Nitrate.nitrite..mg.N.L.) %>%
+  rename(id = ID..., no3mgL = Nitrate.nitrite..mg.N.L.) %>%
+  mutate(id = as.numeric(as.character(id))) %>%
+  filter(!is.na(id))
+
+gw2016 <- rbind(gw2016, gw2016a)
+
+gw2016codes <- xlsx::read.xlsx("../data-raw/water_quality/2016/CopyofSTRIPSgroundwaternitrateresultsv2.xlsx", sheetName = "Lab Codes") %>%
+  select(-Sample.Source) %>%
+  rename(id = ID., position = Position, site = Site, date = Date) %>%
+  mutate(year = "2016",
+         month = 12,
+         position = gsub("TRT-Top", "TRT-top", position),
+         position = gsub("TRT-Bot", "TRT-bot", position),
+         position = gsub("Toe-Top", "TRT-top", position),
+         position = gsub("Toe-Bot", "TRT-bot", position),
+         position = gsub("CTL", "CTL-bot", position),
+         position = gsub("CTL-bot-Bot", "CTL-bot", position),
+         position = gsub("CTL-bot-Top", "CTL-top", position)) %>%
+  filter(!is.na(site)) %>%
+  left_join(gw2016, by = c("id"))
+# 
+# gw2016codesa <- xlsx::read.xlsx("../data-raw/water_quality/2016/CopyofSTRIPSgroundwaternitrateresults.xlsx", sheetName = "Lab Codes") %>%
+#   select(-Sample.Source) %>%
+#   rename(id = ID., position = Position, site = Site, date = Date) %>%
+#   mutate(year = "2016",
+#          month = 12,
+#          position = gsub("TRT-Top", "TRT-top", position),
+#          position = gsub("TRT-Bot", "TRT-bot", position),
+#          position = gsub("Toe-Top", "TRT-top", position),
+#          position = gsub("Toe-Bot", "TRT-bot", position),
+#          position = gsub("CTL", "CTL-bot", position),
+#          position = gsub("CTL-bot-Bot", "CTL-bot", position),
+#          position = gsub("CTL-bot-Top", "CTL-top", position),
+#          date = gsub("March 2016", "March", date)) %>%
+#   left_join(gw2016a, by = c("id")) %>%
+#   filter(!is.na(site))
+# 
+# gw2016codes <- rbind(gw2016codes, gw2016codesa)
+
+gw2016codes$year[gw2016codes$id < 5000 ] = "2015"
+gw2016codes$date[gw2016codes$id < 5000 ] <- "Dec."
+gw2016codes$date[gw2016codes$id < 4200 ] <- "Sept."
+gw2016codes$month[gw2016codes$date == "March" ] <- 3
+gw2016codes$month[gw2016codes$date == "April" ] <- 4
+gw2016codes$month[gw2016codes$date == "May" ] <- 5
+gw2016codes$month[gw2016codes$date == "June" ] <- 6
+gw2016codes$month[gw2016codes$date == "July" ] <- 7
+gw2016codes$month[gw2016codes$date == "Aug." ] <- 8
+gw2016codes$month[gw2016codes$date == "Sept." ] <- 9
+gw2016codes$month[gw2016codes$date == "Oct." ] <- 10
+gw2016codes$month[gw2016codes$date == "Nov." ] <- 11
+#gw2016codes$newdate <- paste(gw2016codes$date, gw2016codes$year)
+gw2016codes$sitepos <- paste(gw2016codes$site, gw2016codes$position)
+gw2016codes$drpmgL <- "NA"
+gw2016codes$sitepos[gw2016codes$sitepos== "Guthrie Top"] <- "Guthrie TRT-top"
+gw2016codes$sitepos[gw2016codes$sitepos== "Guthrie Bottom"] <- "Guthrie TRT-bot"
+# gw2016codes <- gw2016codes %>% 
+#   select(-date)
+
+
+
+
+
+gw2017 <- xlsx::read.xlsx("../data-raw/water_quality/2017/STRIPS2017groundwaterresults.xlsx", sheetName = "Sheet1", startRow = 3) %>%
+  select(Sample.ID., NOx.result..mg.N.L., DRP.result..mg.P.L.) %>%
+  rename(id = Sample.ID., no3mgL = NOx.result..mg.N.L., drpmgL = DRP.result..mg.P.L.)
+
+gw2017codes <- xlsx::read.xlsx("../data-raw/water_quality/2017/STRIPS2017groundwatercodes.xlsx", sheetName = "STRIPS", startRow = 3) %>%
+  select(year, month, site, trt, position, Date, ID.) %>%
+  rename(id = ID., newdate = Date) %>%
+  mutate(year = "2017") %>%
+  left_join(gw2017, by = c("id")) %>%
+  mutate(trt = gsub("trt", "TRT", trt),
+         trt = gsub("ctl", "CTL", trt),
+         site = gsub("rhodes", "Rhodes", site),
+         site = gsub("spirit", "Spirit Lake", site),
+         site = gsub("worle", "Worle", site),
+         site = gsub("white", "Whiterock", site),
+         site = gsub("eia", "EIA", site),
+         site = gsub("mcnay", "McNay", site),
+         site = gsub("arm", "Arm.", site),
+         site = gsub("White", "Whiterock", site),
+         site = gsub("Whiterockrock", "Whiterock", site),
+         site = gsub("guthrie", "Guthrie", site),
+         position = paste(trt, position, sep="-"),
+         sitepos = paste(site, position)) %>%
+  separate(newdate, c("date", "year1"), " ") %>%
+  filter(!is.na(site)) %>%
+  select(-trt, -year1)
+ 
+
+all <- rbind(gw2016codes, gw2017codes) %>%
+  filter(site != "NA") %>%
+  arrange(year, month) %>%
+  mutate(position = gsub("-", "", position),
+         site = gsub("Arm.", "Armstrong", site),
+         date = gsub("August", "Aug.", date),
+         date = gsub("September", "Sept.", date),
+         trt = position,
+         trt = gsub("CTLbot", "CTL", trt),
+         trt = gsub("CTLtop", "CTL", trt),
+         trt = gsub("TRTtop", "TRT", trt),
+         trt = gsub("TRTbot", "TRT", trt),
+         trt = gsub("Bottom", "TRT", trt),
+         trt = gsub("Top", "TRT", trt),
+         trt = gsub("TRTBot", "TRT", trt),
+         trt = gsub("TRTTRT", "TRT", trt),
+         trt = factor(trt),
+         pos = position,
+         pos = gsub("CTLbot", "Bottom", pos),
+         pos = gsub("TRTbot", "Bottom", pos),
+         pos = gsub("CTLtop", "Top", pos),
+         pos = gsub("TRTtop", "Top", pos),
+         pos = gsub("TRTBot", "Bottom", pos),
+         pos = factor(pos),
+        position = factor(position, 
+                          levels = c("CTLbot","TRTbot","TRTtop", "CTLtop", "Top", "Bottom"))
+  )
+
+all$order <- factor(all$date, levels = c("Feb.", "March", "April", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."))
+
+# Customized colors
+colorscale <- c(TRT = "blue", 
+                CTL = "red")
+linescale <- c(Top = "dashed",
+               Bottom = "solid")
+
+
+no3 <- ggplot(all, aes(x = order, 
+                   y = no3mgL, 
+                   group = position,
+                  linetype = pos,
+                 color = trt)) +
+  geom_line() + 
+  geom_point() +
+  facet_grid(site~year, scales='free_x') + 
+  labs(x = '',  
+       y = 'Nitrate and Nitrite (mg/L)') + 
+  scale_color_manual(values = colorscale) +
+  scale_linetype_manual(values = linescale) +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        legend.title    = element_blank(),
+        axis.text.x = element_text(angle=60,hjust=1))
+
+ggsave(filename = "gwno3.jpg", plot=no3, width = 6, height=8)
+
+drpdata <- filter(all, year=="2017", drpmgL!="NA")
+drpdata$drpmgL <- as.numeric(drpdata$drpmgL)
+
+drp <- ggplot(drpdata, aes(x = order, 
+                   y = drpmgL, 
+                   group = position,
+                   linetype = pos,
+                   color = trt)) +
+  geom_line() + 
+  geom_point() +
+  facet_grid(site~year, scales='free_x') + 
+  labs(x = '',  
+       y = 'Dissolved Reactive Phosphorus (mg/L)') + 
+  scale_color_manual(values = colorscale) +
+  scale_linetype_manual(values = linescale) +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        legend.title    = element_blank(),
+        axis.text.x = element_text(angle=60,hjust=1))
+
+ggsave(filename = "gwdrp.jpg", plot=drp, width = 6, height=8)
+
+########################################################################################
+
+gwdepth <- xlsx::read.xlsx("../data-raw/water_quality/2016/Depth to Groundwater.xlsx", sheetName = "strips 2") %>%
+  select(-NA.) %>%
+  filter(!is.na(site)) %>%
+  rename(rawdepthft = uncorrected.depth..ft.) %>%
+  mutate(site = gsub("Arm", "Armstrong", site),
+         site = gsub("arm", "Armstrong", site),
+         site = gsub("white", "Whiterock", site),
+         site = gsub("white ", "Whiterock", site),
+         site = gsub("spirit", "Spirit Lake", site),
+         site = gsub("eia", "EIA", site),
+         site = gsub("guthrie", "Guthrie", site),
+         site = gsub("mcnay", "McNay", site),
+         site = gsub("rhodes", "Rhodes", site),
+         site = gsub("worle", "Worle", site),
+         pos = gsub("top", "Top", pos),
+         pos = gsub("bot", "Bottom", pos),
+         trt = gsub("trt", "TRT", trt),
+         trt = gsub("ctl", "CTL", trt),
+         position = paste(trt, pos),
+         sitepos = paste(site, position),
+         #make date column (month names)
+         #correct for well above ground
+         abovegroundin = 1.5)
+
+gwdepth$abovegroundin[gwdepth$sitepos=="Whiterock CTL Bottom"] <- 2
+gwdepth$abovegroundin[gwdepth$sitepos=="Spirit Lake TRT Top"] <- 1
+gwdepth$abovegroundin[gwdepth$sitepos=="Spirit Lake CTL Top"] <- 13.8 
+gwdepth$abovegroundin[gwdepth$sitepos=="Spirit Lake CTL Bottom"] <- 0
+gwdepth$abovegroundin[gwdepth$sitepos=="Armstrong TRT Top"] <- 2.5
+gwdepth$abovegroundin[gwdepth$sitepos=="Armstrong TRT Bottom"] <- 4
+gwdepth$abovegroundin[gwdepth$sitepos=="Armstrong CTL Bottom"] <- 0
+gwdepth$abovegroundin[gwdepth$sitepos=="Worle TRT Top"] <- 0
+gwdepth$abovegroundin[gwdepth$sitepos=="Worle TRT Bottom"] <- 2
+gwdepth$abovegroundin[gwdepth$sitepos=="McNay CTL Bottom"] <- 1
+gwdepth$abovegroundin[gwdepth$sitepos=="EIA TRT Top"] <- 1
+gwdepth$abovegroundin[gwdepth$sitepos=="EIA TRT Bottom"] <- 0.5
+gwdepth$abovegroundin[gwdepth$sitepos=="Rhodes TRT Top"] <- 5
+gwdepth$abovegroundin[gwdepth$sitepos=="Rhodes TRT Bottom"] <- 0
+gwdepth$abovegroundin[gwdepth$sitepos=="Rhodes CTL Bottom"] <- 2
+gwdepth$abovegroundin[gwdepth$sitepos=="Guthrie TRT Top"] <- 0
+gwdepth$abovegroundin[gwdepth$sitepos=="Guthrie TRT Bottom"] <- 2
+
+gwdepth <- gwdepth %>%
+  mutate(adjdepthft = as.numeric(as.character(rawdepthft))-(abovegroundin/12))
+  
+gwdepth$adjdepthft <- as.numeric(format(round(gwdepth$adjdepthft, 2), nsmall = 2))
+
+gwdepth$negadjdepthft <- -(gwdepth$adjdepthft)
+
+gwdepth$date<- "Feb."
+gwdepth$date[gwdepth$month=="3"] <- "March"
+gwdepth$date[gwdepth$month=="4"] <- "April"
+gwdepth$date[gwdepth$month=="5"] <- "May"
+gwdepth$date[gwdepth$month=="6"] <- "June"
+gwdepth$date[gwdepth$month=="7"] <- "July"
+gwdepth$date[gwdepth$month=="8"] <- "Aug."
+gwdepth$date[gwdepth$month=="9"] <- "Sept."
+gwdepth$date[gwdepth$month=="10"] <- "Oct."
+gwdepth$date[gwdepth$month=="11"] <- "Nov."
+gwdepth$date[gwdepth$month=="12"] <- "Dec."
+
+
+gwdepth$order <- factor(gwdepth$date, levels = c("Feb.", "March", "April", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."))
+
+
+gwdepthplot <- ggplot(gwdepth, aes(x = order, 
+                           y = negadjdepthft, 
+                           group = position,
+                           linetype = pos,
+                           color = trt)) +
+  geom_line() + 
+  geom_point() +
+  facet_grid(site~year, scales='free_x') + 
+  labs(x = '',  
+       y = 'Groundwater Depth From Ground Surface (ft)') + 
+  scale_color_manual(values = colorscale) +
+  scale_linetype_manual(values = linescale) +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        legend.title    = element_blank(),
+        axis.text.x = element_text(angle=60,hjust=1))
+
+ggsave(filename = "gwdepth.jpg", plot=gwdepthplot, width = 6, height=8)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+######################################################################
+#THIS WAS FOR LISA TO GIVE TO EIA, also will edit to give to Hoien for spirit lake
+
+
+
+spirit <- all %>%
+  filter(site=="Spirit Lake")
+
+spiritno3plot <- ggplot(spirit, aes(x = order, 
+                          y = no3mgL, 
+                          group = position,
+                          linetype = pos,
+                          color = trt)) +
+  ggtitle("Groundwater Nitrate") +
+  geom_line() + 
+  geom_point() +
+  facet_grid(site~year, scales='free_x') + 
+  labs(x = '',  
+       y = 'Nitrate and Nitrite (mg/L)') + 
+  scale_color_manual(values = colorscale) +
+  scale_linetype_manual(values = linescale) +
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(legend.position = "bottom",
+        legend.title    = element_blank(),
+        axis.text.x = element_text(angle=60,hjust=1))
+
+ggsave(filename = "gwspiritno3.jpg", plot=spiritno3plot, width = 6, height=8)
+
+
+spiritdrp <- drpdata %>%
+  filter(site=="Spirit Lake")
+
+spiritdrpplot <- ggplot(spiritdrp, aes(x = order, 
+                           y = drpmgL, 
+                           group = position,
+                           linetype = pos,
+                           color = trt)) +
+  ggtitle("Groundwater Dissolved Reactive Phosphorus") +
+  geom_line() + 
+  geom_point() +
+  facet_grid(site~year, scales='free_x') + 
+  labs(x = '',  
+       y = 'Dissolved Reactive Phosphorus (mg/L)') + 
+  scale_color_manual(values = colorscale) +
+  scale_linetype_manual(values = linescale) +
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(legend.position = "bottom",
+        legend.title    = element_blank(),
+        axis.text.x = element_text(angle=60,hjust=1))
+
+ggsave(filename = "gwspiritdrp.jpg", plot=spiritdrpplot, width = 6, height=8)
+
+
+spiritgwdepth <- gwdepth %>%
+  filter(site=="Spirit Lake")
+
+spiritgwdepthplot <- ggplot(spiritgwdepth, aes(x = order, 
+                                   y = negadjdepthft, 
+                                   group = position,
+                                   linetype = pos,
+                                   color = trt)) +
+  ggtitle("Groundwater Depth from Ground Surface") +
+  geom_line() + 
+  geom_point() +
+  facet_grid(site~year, scales='free_x') + 
+  labs(x = '',  
+       y = 'Groundwater Depth From Ground Surface (ft)') + 
+  scale_color_manual(values = colorscale) +
+  scale_linetype_manual(values = linescale) +
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(legend.position = "bottom",
+        legend.title    = element_blank(),
+        axis.text.x = element_text(angle=60,hjust=1))
+
+ggsave(filename = "spiritgwdepth.jpg", plot=spiritgwdepthplot, width = 6, height=8)
+
+
+
+
+
+##########################################################################
