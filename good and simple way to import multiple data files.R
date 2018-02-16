4184ebec6ea1a0e3cfaf42e5406b303ca4f1c193
wittec <witte@iastate.edu>
2018-02-16 13:24:20 -0600

    Added some comments

--- good and simple way to import multiple data files.R
+++ good and simple way to import multiple data files.R
@@ -4,7 +4,7 @@ rm(list=ls(all=T))
 
 library("tidyverse")
 
-pr2files <- list.files("./", pattern = "pr*")
+pr2files <- list.files("./", pattern = "pr*") #THIS CREATES A LIST OF FILES I WANT TO IMPORT
 
 importing <- function(t) {
                           name <- t
@@ -13,7 +13,7 @@ importing <- function(t) {
                           file
 }
 
-data <- plyr::ldply(pr2files, importing) %>%
+data <- plyr::ldply(pr2files, importing) %>%          #THIS RUNS LDPLY ON THE LIST OF FILES USING MY IMPORTING FUNCTION
 
 select(name, X1, X2, X5, X7, X9, X11, X13, X15) %>%
 rename(datetime = X1, measnum = X2, mv100 = X5, mv200 = X7, mv300 = X9, mv400 = X11, mv600 = X13, mv1000 = X15)
