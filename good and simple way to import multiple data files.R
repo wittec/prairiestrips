63f29757670e8606752130a2a4a640cdccb91c7f
wittec <witte@iastate.edu>
2018-02-16 13:22:01 -0600

    Came up with this myself

---
+++ good and simple way to import multiple data files.R
@@ -0,0 +1,19 @@
+# setwd("C:/Users/Chris/Box Sync/Documents/Data/soil moisture/raw data/2013")
+
+rm(list=ls(all=T))
+
+library("tidyverse")
+
+pr2files <- list.files("./", pattern = "pr*")
+
+importing <- function(t) {
+                          name <- t
+                          file <- readr::read_csv(t, skip = 38, col_names = F)
+                          file$name <- name
+                          file
+}
+
+data <- plyr::ldply(pr2files, importing) %>%
+
+select(name, X1, X2, X5, X7, X9, X11, X13, X15) %>%
+rename(datetime = X1, measnum = X2, mv100 = X5, mv200 = X7, mv300 = X9, mv400 = X11, mv600 = X13, mv1000 = X15)
