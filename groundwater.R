b23485c41ac871c747b2d4dfd0a35b554b6fd28f
wittec <witte@iastate.edu>
2018-08-03 14:25:04 -0500

    Fixed the program using the correcting csv file to fix errors in the WATER QUALITY part of program... next to do the depth section.

--- groundwater.R
+++ groundwater.R
@@ -1,46 +1,31 @@
 # setwd("~/prairiestrips/groundwater/data-raw/waterquality/")
 
-rm(list=ls(all=TRUE))
-
-
-library("dplyr")
-library("tidyr")
-library("ggplot2")
-
-
-gw2016 <- read.csv("./2016/2016gwno3results2.csv", skip = 21, header = T) %>%
-  select(ID..., NOx.result..mg.N.L.) %>%
-  rename(id = ID..., no3mgL = NOx.result..mg.N.L.) %>%
-  mutate(id = as.numeric(as.character(id))) %>%
-  filter(!is.na(id))
-
-
-gw2016a <- read.csv("./2016/2016gwno3results.csv", skip = 20, header = T) %>%
-  select(ID..., Nitrate.nitrite..mg.N.L.) %>%
-  rename(id = ID..., no3mgL = Nitrate.nitrite..mg.N.L.) %>%
-  mutate(id = as.numeric(as.character(id))) %>%
-  filter(!is.na(id))
-
-gw2016 <- rbind(gw2016, gw2016a)
-
-gw2016codes <- read.csv("2016/2016gwcodes.csv", header = T) %>%
-  select(-Sample.Source) %>%
-  filter(!is.na(ID.)) %>%
-  rename(id = ID., position = Position, site = Site, date = Date) %>%
-  mutate(year = "2016",
-         month = 12,
-         position = gsub("TRT-Top", "TRT-top", position),
-         position = gsub("TRT-Bot", "TRT-bot", position),
-         position = gsub("Toe-Top", "TRT-top", position),
-         position = gsub("Toe-Bot", "TRT-bot", position),
-         position = gsub("CTL", "CTL-bot", position),
-         position = gsub("CTL-bot-Bot", "CTL-bot", position),
-         position = gsub("CTL-bot-Top", "CTL-top", position)) %>%
-  filter(!is.na(site)) %>%
-  left_join(gw2016, by = c("id"))
+# rm(list=ls(all=TRUE))
 # 
-# gw2016codesa <- xlsx::read.xlsx("../data-raw/water_quality/2016/CopyofSTRIPSgroundwaternitrateresults.xlsx", sheetName = "Lab Codes") %>%
+# 
+# library("dplyr")
+# library("tidyr")
+# library("ggplot2")
+# 
+# 
+# gw2016 <- read.csv("./2016/2016gwno3results2.csv", skip = 21, header = T) %>%
+#   select(ID..., NOx.result..mg.N.L.) %>%
+#   rename(id = ID..., no3mgL = NOx.result..mg.N.L.) %>%
+#   mutate(id = as.numeric(as.character(id))) %>%
+#   filter(!is.na(id))
+# 
+# 
+# gw2016a <- read.csv("./2016/2016gwno3results.csv", skip = 20, header = T) %>%
+#   select(ID..., Nitrate.nitrite..mg.N.L.) %>%
+#   rename(id = ID..., no3mgL = Nitrate.nitrite..mg.N.L.) %>%
+#   mutate(id = as.numeric(as.character(id))) %>%
+#   filter(!is.na(id))
+# 
+# gw2016 <- rbind(gw2016, gw2016a)
+# 
+# gw2016codes <- read.csv("2016/2016gwcodes.csv", header = T) %>%
 #   select(-Sample.Source) %>%
+#   filter(!is.na(ID.)) %>%
 #   rename(id = ID., position = Position, site = Site, date = Date) %>%
 #   mutate(year = "2016",
 #          month = 12,
@@ -50,64 +35,83 @@ gw2016codes <- read.csv("2016/2016gwcodes.csv", header = T) %>%
 #          position = gsub("Toe-Bot", "TRT-bot", position),
 #          position = gsub("CTL", "CTL-bot", position),
 #          position = gsub("CTL-bot-Bot", "CTL-bot", position),
-#          position = gsub("CTL-bot-Top", "CTL-top", position),
-#          date = gsub("March 2016", "March", date)) %>%
-#   left_join(gw2016a, by = c("id")) %>%
-#   filter(!is.na(site))
+#          position = gsub("CTL-bot-Top", "CTL-top", position)) %>%
+#   filter(!is.na(site)) %>%
+#   left_join(gw2016, by = c("id"))
+# # 
+# # gw2016codesa <- xlsx::read.xlsx("../data-raw/water_quality/2016/CopyofSTRIPSgroundwaternitrateresults.xlsx", sheetName = "Lab Codes") %>%
+# #   select(-Sample.Source) %>%
+# #   rename(id = ID., position = Position, site = Site, date = Date) %>%
+# #   mutate(year = "2016",
+# #          month = 12,
+# #          position = gsub("TRT-Top", "TRT-top", position),
+# #          position = gsub("TRT-Bot", "TRT-bot", position),
+# #          position = gsub("Toe-Top", "TRT-top", position),
+# #          position = gsub("Toe-Bot", "TRT-bot", position),
+# #          position = gsub("CTL", "CTL-bot", position),
+# #          position = gsub("CTL-bot-Bot", "CTL-bot", position),
+# #          position = gsub("CTL-bot-Top", "CTL-top", position),
+# #          date = gsub("March 2016", "March", date)) %>%
+# #   left_join(gw2016a, by = c("id")) %>%
+# #   filter(!is.na(site))
+# # 
+# # gw2016codes <- rbind(gw2016codes, gw2016codesa)
 # 
-# gw2016codes <- rbind(gw2016codes, gw2016codesa)
+# gw2016codes$year[gw2016codes$id < 5000 ] = "2015"
+# gw2016codes$date[gw2016codes$id < 5000 ] <- "Dec."
+# gw2016codes$date[gw2016codes$id < 4200 ] <- "Sept."
+# gw2016codes$month[gw2016codes$date == "March" ] <- 3
+# gw2016codes$month[gw2016codes$date == "April" ] <- 4
+# gw2016codes$month[gw2016codes$date == "May" ] <- 5
+# gw2016codes$month[gw2016codes$date == "June" ] <- 6
+# gw2016codes$month[gw2016codes$date == "July" ] <- 7
+# gw2016codes$month[gw2016codes$date == "Aug." ] <- 8
+# gw2016codes$month[gw2016codes$date == "Sept." ] <- 9
+# gw2016codes$month[gw2016codes$date == "Oct." ] <- 10
+# gw2016codes$month[gw2016codes$date == "Nov." ] <- 11
+# #gw2016codes$newdate <- paste(gw2016codes$date, gw2016codes$year)
+# gw2016codes$sitepos <- paste(gw2016codes$site, gw2016codes$position)
+# gw2016codes$drpmgL <- "NA"
+# gw2016codes$sitepos[gw2016codes$sitepos== "Guthrie Top"] <- "Guthrie TRT-top"
+# gw2016codes$sitepos[gw2016codes$sitepos== "Guthrie Bottom"] <- "Guthrie TRT-bot"
+# # gw2016codes <- gw2016codes %>% 
+# #   select(-date)
+# 
+# 
+# 
+# 
+# 
+# gw2017 <- read.csv("./2017/STRIPS2017groundwaterresults.csv", skip = 2, header = T) %>%
+#   select(Sample.ID., NOx.result..mg.N.L., DRP.result..mg.P.L.) %>%
+#   rename(id = Sample.ID., no3mgL = NOx.result..mg.N.L., drpmgL = DRP.result..mg.P.L.)
+# 
+# gw2017codes <- read.csv("./2017/STRIPS2017groundwatercodes.csv", skip = 2) %>%
+#   select(year, month, site, trt, position, ID.) %>%
+#   rename(id = ID.) %>%
+#   filter(id != "NA") %>%
+#   left_join(gw2017, by = c("id")) %>%
+#   mutate(trt = gsub("trt", "TRT", trt),
+#          trt = gsub("ctl", "CTL", trt),
+#          site = gsub("rhodes", "Rhodes", site),
+#          site = gsub("spirit", "Spirit Lake", site),
+#          site = gsub("worle", "Worle", site),
+#          site = gsub("white", "Whiterock", site),
+#          site = gsub("eia", "EIA", site),
+#          site = gsub("mcnay", "McNay", site),
+#          site = gsub("arm", "Arm.", site),
+#          site = gsub("White", "Whiterock", site),
+#          site = gsub("Whiterockrock", "Whiterock", site),
+#          site = gsub("guthrie", "Guthrie", site),
+#          position = paste(trt, position, sep="-"),
+#          sitepos = paste(site, position)) %>%
+#   separate(newdate, c("date", "year1"), " ") %>%
+#   filter(!is.na(site)) %>%
+#   select(-trt, -year1)
+#  
 
-gw2016codes$year[gw2016codes$id < 5000 ] = "2015"
-gw2016codes$date[gw2016codes$id < 5000 ] <- "Dec."
-gw2016codes$date[gw2016codes$id < 4200 ] <- "Sept."
-gw2016codes$month[gw2016codes$date == "March" ] <- 3
-gw2016codes$month[gw2016codes$date == "April" ] <- 4
-gw2016codes$month[gw2016codes$date == "May" ] <- 5
-gw2016codes$month[gw2016codes$date == "June" ] <- 6
-gw2016codes$month[gw2016codes$date == "July" ] <- 7
-gw2016codes$month[gw2016codes$date == "Aug." ] <- 8
-gw2016codes$month[gw2016codes$date == "Sept." ] <- 9
-gw2016codes$month[gw2016codes$date == "Oct." ] <- 10
-gw2016codes$month[gw2016codes$date == "Nov." ] <- 11
-#gw2016codes$newdate <- paste(gw2016codes$date, gw2016codes$year)
-gw2016codes$sitepos <- paste(gw2016codes$site, gw2016codes$position)
-gw2016codes$drpmgL <- "NA"
-gw2016codes$sitepos[gw2016codes$sitepos== "Guthrie Top"] <- "Guthrie TRT-top"
-gw2016codes$sitepos[gw2016codes$sitepos== "Guthrie Bottom"] <- "Guthrie TRT-bot"
-# gw2016codes <- gw2016codes %>% 
-#   select(-date)
 
 
 
-
-
-gw2017 <- read.csv("./2017/STRIPS2017groundwaterresults.csv", skip = 2, header = T) %>%
-  select(Sample.ID., NOx.result..mg.N.L., DRP.result..mg.P.L.) %>%
-  rename(id = Sample.ID., no3mgL = NOx.result..mg.N.L., drpmgL = DRP.result..mg.P.L.)
-
-gw2017codes <- read.csv("./2017/STRIPS2017groundwatercodes.csv", skip = 2) %>%
-  select(year, month, site, trt, position, ID.) %>%
-  rename(id = ID.) %>%
-  filter(id != "NA") %>%
-  left_join(gw2017, by = c("id")) %>%
-  mutate(trt = gsub("trt", "TRT", trt),
-         trt = gsub("ctl", "CTL", trt),
-         site = gsub("rhodes", "Rhodes", site),
-         site = gsub("spirit", "Spirit Lake", site),
-         site = gsub("worle", "Worle", site),
-         site = gsub("white", "Whiterock", site),
-         site = gsub("eia", "EIA", site),
-         site = gsub("mcnay", "McNay", site),
-         site = gsub("arm", "Arm.", site),
-         site = gsub("White", "Whiterock", site),
-         site = gsub("Whiterockrock", "Whiterock", site),
-         site = gsub("guthrie", "Guthrie", site),
-         position = paste(trt, position, sep="-"),
-         sitepos = paste(site, position)) %>%
-  separate(newdate, c("date", "year1"), " ") %>%
-  filter(!is.na(site)) %>%
-  select(-trt, -year1)
- 
 
 # reworking stuff above to be more streamlined ----------------------------
 
@@ -151,14 +155,26 @@ gw2017 <- read.csv("./2017/STRIPS2017groundwaterresults.csv", skip = 2, header =
 
 gw2016 <- gw2016 %>%
   rbind(gw2016a) %>%
-  full_join(gw2016codes)
+  full_join(gw2016codes) %>%
+  rename(month = date) %>%
+  mutate(month = as.character(month))
 
 gw2017 <- gw2017 %>%
-  full_join(gw2017codes)
+  full_join(gw2017codes) %>%
+  mutate(month = as.character(month))
 
 all <- gw2016 %>%
   full_join(gw2017) %>%
-  mutate(date = as.character(date))
+  filter(!is.na(id)) %>%
+  filter(!is.na(site))
+
+all$trt[all$position == "CTL" | all$position == "CTL-Bot" | all$position == "CTL-Top"] <- "CTL"
+all$trt[is.na(all$trt)] <- "TRT"
+all$position[all$position == "CTL"] <- "Bot"
+is.na(all$id) <- NULL
+all$year[all$id <5000] <- "2015"
+all$year[all$id >= 5000 & all$id < 6000] <- "2016"
+all$year[all$id >= 6000] <- "2017"
 
 # correcting typo's, wrong codes, etc. ------------------------------------
 
@@ -169,48 +185,22 @@ fixit <- function (x) {
      )
 
    testdata <- as.data.frame(x)
-   
-  # testdata$totest <- x #%>%
-  #x <- as.data.frame(x)  %>%
-    #%>%
-   
+  
   left_join(testdata, correct, by = c("x" = "bad"))  %>%
   mutate(x = ifelse(!is.na(good), good, x)) %>%
-  # 
-  #  x
-  
-  #testdata <- testdata %>%
   select(-ends_with("good"))
   
 }
 
-corrected <- as.data.frame(lapply(all[3:5], fixit)) %>%
-  rename(site = x, position = x.1, date = x.2)
-
+corrected <- as.data.frame(lapply(all[c(3:5,8)], fixit)) %>%
+  rename(site = x, position = x.1, month = x.2, trt = x.3)
+  
 all$site <- corrected$site
 all$position <- corrected$position
-all$date <- corrected$date
-
-
-
-
-rbind(gw2016, gw2017) %>%
-  left_join(gw2016codes, by = c("id")) %>%
-  left_join(gw2017codes, by = c("id"))
-
-
-gw2016 <- rbind(gw2016, gw2016a) %>%
-  left_join(gw2016codes) %>%
-  separate(date, c("month", "year"), " ") %>%
-  mutate(
-    month = gsub("Sept.", "September", month),
-    month = gsub("Dec.", "December", month),
-    month = gsub("Aug.", "August", month),
-    month = gsub("Oct.", "October", month),
-    month = gsub("Nov.", "November", month)
-  )
-
-gw2016$year <- gw2016$year %>%  replace_na("2016")
+all$month <- corrected$month
+all$trt <- corrected$trt
+all <- all %>%
+  mutate(pos = paste(trt, position, sep = "-"))
 
 
 
@@ -225,60 +215,63 @@ gw2016$year <- gw2016$year %>%  replace_na("2016")
 
 
 
+# 
+# all <- rbind(gw2016codes, gw2017codes) %>%
+#   filter(site != "NA") %>%
+#   arrange(year, month) %>%
+#   mutate(position = gsub("-", "", position),
+#          site = gsub("Arm.", "Armstrong", site),
+#          date = gsub("August", "Aug.", date),
+#          date = gsub("September", "Sept.", date),
+#          date = gsub("October", "Oct.", date),
+#          date = gsub("November", "Nov.", date),
+#          date = gsub("December", "Dec.", date),
+#          trt = position,
+#          trt = gsub("CTLbot", "CTL", trt),
+#          trt = gsub("CTLtop", "CTL", trt),
+#          trt = gsub("TRTtop", "TRT", trt),
+#          trt = gsub("TRTbot", "TRT", trt),
+#          trt = gsub("Bottom", "TRT", trt),
+#          trt = gsub("Top", "TRT", trt),
+#          trt = gsub("TRTBot", "TRT", trt),
+#          trt = gsub("TRTTRT", "TRT", trt),
+#          trt = factor(trt),
+#          pos = position,
+#          pos = gsub("CTLbot", "Bottom", pos),
+#          pos = gsub("TRTbot", "Bottom", pos),
+#          pos = gsub("CTLtop", "Top", pos),
+#          pos = gsub("TRTtop", "Top", pos),
+#          pos = gsub("TRTBot", "Bottom", pos),
+#          pos = factor(pos),
+#         position = factor(position, 
+#                           levels = c("CTLbot","TRTbot","TRTtop", "CTLtop", "Top", "Bottom"))
+#   )
 
-
-
-
-
-all <- rbind(gw2016codes, gw2017codes) %>%
-  filter(site != "NA") %>%
-  arrange(year, month) %>%
-  mutate(position = gsub("-", "", position),
-         site = gsub("Arm.", "Armstrong", site),
-         date = gsub("August", "Aug.", date),
-         date = gsub("September", "Sept.", date),
-         date = gsub("October", "Oct.", date),
-         date = gsub("November", "Nov.", date),
-         date = gsub("December", "Dec.", date),
-         trt = position,
-         trt = gsub("CTLbot", "CTL", trt),
-         trt = gsub("CTLtop", "CTL", trt),
-         trt = gsub("TRTtop", "TRT", trt),
-         trt = gsub("TRTbot", "TRT", trt),
-         trt = gsub("Bottom", "TRT", trt),
-         trt = gsub("Top", "TRT", trt),
-         trt = gsub("TRTBot", "TRT", trt),
-         trt = gsub("TRTTRT", "TRT", trt),
-         trt = factor(trt),
-         pos = position,
-         pos = gsub("CTLbot", "Bottom", pos),
-         pos = gsub("TRTbot", "Bottom", pos),
-         pos = gsub("CTLtop", "Top", pos),
-         pos = gsub("TRTtop", "Top", pos),
-         pos = gsub("TRTBot", "Bottom", pos),
-         pos = factor(pos),
-        position = factor(position, 
-                          levels = c("CTLbot","TRTbot","TRTtop", "CTLtop", "Top", "Bottom"))
-  )
-
-all$order <- factor(all$date, levels = c("Feb.", "March", "April", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."))
+all$order <- factor(all$month, levels = c("Feb.", "Mar.", "Apr.", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."))
 
 # Customized colors
 colorscale <- c(TRT = "blue", 
-                CTL = "red")
+                CTL = "red",
+                MCL = "green")
 linescale <- c(Top = "dashed",
-               Bottom = "solid")
+               Bot = "solid",
+               MCL = "solid")
 
 no3data <- all %>%
   filter(no3mgL!="NA" & year != "2015")
 
+
+###############LEFT OFF HERE
+
 no3 <- ggplot(no3data, aes(x = order, 
                    y = no3mgL, 
-                   group = position,
-                  linetype = pos,
+                   group = pos,
+                  linetype = position,
                  color = trt)) +
-  geom_line() + 
-  geom_point() +
+  geom_line(size = 1) + 
+  geom_point(size= 1.5) +
+  #add in next line for max concentration limit for drinking water 
+  #geom_hline(aes(color = "MCL", yintercept = 10)) +
   facet_grid(site~year, scales='free_x') + 
   labs(x = '',  
        y = 'Groundwater Nitrate - Nitrogen (mg/L)') + 
@@ -298,11 +291,11 @@ drpdata$drpmgL <- as.numeric(drpdata$drpmgL)
 
 drp <- ggplot(drpdata, aes(x = order, 
                    y = drpmgL, 
-                   group = position,
-                   linetype = pos,
+                   group = pos,
+                   linetype = position,
                    color = trt)) +
-  geom_line() + 
-  geom_point() +
+  geom_line(size = 1) + 
+  geom_point(size= 1.5) +
   facet_grid(site~year, scales='free_x') + 
   labs(x = '',  
        y = 'Groundwater Dissolved Phosphorus (mg/L)') + 
